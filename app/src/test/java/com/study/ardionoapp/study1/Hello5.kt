9e66e2a8f2fa97b098a0da11948ef9ac3aaf21a2
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-05 11:14:41 +0800

    App

--- app/src/test/java/com/study/ardionoapp/study1/Hello5.kt
+++ app/src/test/java/com/study/ardionoapp/study1/Hello5.kt
@@ -10,13 +10,24 @@ package com.study.ardionoapp.study1
     74/(4-2) = 37 .. 兔子
     83 - 37  = 46 .. 雞
 */
-fun main() {
-    val amount = 83
-    val feets = 240
-
+fun printResult(amount: Int, feets: Int) {
+    if (feets < amount * 2) {
+        println("資料不正確")
+        return // 不回傳任何值, 方法結束
+    }
+    if (feets > amount * 4) {
+        println("資料不正確")
+        return // 不回傳任何值, 方法結束
+    }
     val rabbit = (feets - (amount * 2))/2
     val chichen = amount - rabbit
 
     println("雞: ${chichen} 兔子: ${rabbit}")
+}
+
+fun main() {
+    printResult(83, 240)
+    printResult(83, 200)
+    printResult(83, 500)
 
 }
