89946f83d5d5fcd37f37bf838f5eb9eb5a933f26
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-05 15:27:15 +0800

    App

--- app/src/test/java/com/study/ardionoapp/study1/HelloFlowControl.kt
+++ app/src/test/java/com/study/ardionoapp/study1/HelloFlowControl.kt
@@ -34,4 +34,22 @@ fun main() {
         else -> "E"
     }
     println(level2)
+
+    // for
+    for (i in 1..10) {
+        print("$i ")
+    }
+    println()
+    for (i in 1..10 step 2) {
+        print("$i ")
+    }
+    println()
+    for (i in 1 until 10) { // 不含 10
+        print("$i ")
+    }
+    println()
+    for (i in 10 downTo 1) {
+        print("$i ")
+    }
+    println()
 }
