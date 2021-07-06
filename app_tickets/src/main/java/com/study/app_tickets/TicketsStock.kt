ff71aa92dae6964391ca3ffc6d8be29ceed61081
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-06 11:13:45 +0800

    App

--- app_tickets/src/main/java/com/study/app_tickets/TicketsStock.kt
+++ app_tickets/src/main/java/com/study/app_tickets/TicketsStock.kt
@@ -2,7 +2,7 @@ package com.study.app_tickets
 
 class TicketsStock {
     companion object {
-        var totalAmount = 20
+        var totalAmount = 30
         fun subAmount(amount: Int) {
             totalAmount = totalAmount - amount
         }
