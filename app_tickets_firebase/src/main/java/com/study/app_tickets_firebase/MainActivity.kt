45cdec15d038fe60e4c394bc90adb4af48350f14
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-07 13:16:52 +0800

    App

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/MainActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.study.app_tickets_firebase
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val database = Firebase.database
     val myRef = database.getReference("ticketsStock")
+    lateinit var context: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        context = this
         // Read from the database
         myRef.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -43,4 +47,77 @@ class MainActivity : AppCompatActivity() {
         tv_ticket_discount.setText((TicketsStock.discount * 10).toString())
         tv_total_amount.setText(TicketsStock.totalAmount.toString())
     }
+
+    // 清除欄位資料(按下清除鈕)
+    fun reset(view: View) {
+        refreshUI()
+        et_all_tickets.setText("0")
+        et_round_trip.setText("0")
+    }
+
+    // 驗證購票欄位
+    fun confirmTickets(): Boolean {
+        // 1. 檢查各欄位的資料是否有值, 若沒有值則自動補 0
+        if(et_all_tickets.text == null || et_all_tickets.text.toString().equals("")) {
+            et_all_tickets.setText("0")
+        }
+        if(et_round_trip.text == null || et_round_trip.text.toString().equals("")) {
+            et_round_trip.setText("0")
+        }
+
+        val allTickets = et_all_tickets.text.toString().toInt()
+        val roundTrip  = et_round_trip.text.toString().toInt()
+
+        // 2. 購買票數 > 0
+        if(allTickets <= 0) {
+            tv_warning.setText("購買票數必須 > 0")
+            Toast.makeText(context, "購買票數必須 > 0", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        // 3. 檢查剩餘票數是否足夠 ?
+        val totalAmount = tv_total_amount.text.toString().toInt()
+        if (allTickets > totalAmount) {
+            tv_warning.setText("剩餘票數不足 !")
+            Toast.makeText(context, "剩餘票數不足 !", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        // 4. 來回票組數是否設定正確 ?
+        if(allTickets < roundTrip * 2) {
+            tv_warning.setText("來回票組數過多 !")
+            Toast.makeText(context, "剩餘票數不足 !", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    // 購票流程(按下購買結帳鈕)
+    fun buyTicket(view: View) {
+        // 檢驗票數資訊
+        if(!confirmTickets()) {
+            return
+        }
+
+        val allTickets = et_all_tickets.text.toString().toInt()
+        val roundTrip  = et_round_trip.text.toString().toInt()
+
+        // 進行票務處理
+        val tickets = Tickets(allTickets, roundTrip)
+        //  result = 總票數：%d\n來回票：%d\n單程票：%d\n總金額：$%,d
+        var result = resources.getString(R.string.tickets_result)
+        result = String.format(result,
+            tickets.allTickets,
+            tickets.roundTrip,
+            tickets.oneWay,
+            tickets.total())
+        tv_result.setText(result)
+        // update 剩餘張數
+        TicketsStock.subAmount(tickets.allTickets)
+        tv_total_amount.setText(TicketsStock.totalAmount.toString())
+        // 購買成功訊息
+        tv_warning.setText("購買成功 !")
+        Toast.makeText(context, "購買成功 !", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
