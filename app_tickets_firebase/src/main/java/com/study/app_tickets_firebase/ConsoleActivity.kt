6b7895c80e0ca91252502627567bdf5fca39ca99
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-07 16:23:39 +0800

    App

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/ConsoleActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/ConsoleActivity.kt
@@ -2,10 +2,36 @@ package com.study.app_tickets_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_console.*
+import kotlinx.android.synthetic.main.activity_order_list.*
 
 class ConsoleActivity : AppCompatActivity() {
+    val database = Firebase.database
+    val myRef = database.getReference("ticketsStock")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_console)
+
+        // Read from the database
+        myRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val children = snapshot.children
+                children.forEach {
+                    when(it.key.toString()) {
+                        "discount" -> et_discount.setText(it.value.toString())
+                        "price" -> et_price.setText(it.value.toString())
+                        "totalAmount" -> et_total_amount.setText(it.value.toString())
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 }
--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/LoginActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -24,6 +25,12 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun consoleLogin(view: View) {
-        
+        val userName = et_username.text.toString()
+        if(userName.equals("admin")) {
+            val intent = Intent(context, ConsoleActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(context, "無權限進入 !", Toast.LENGTH_SHORT).show()
+        }
     }
 }
