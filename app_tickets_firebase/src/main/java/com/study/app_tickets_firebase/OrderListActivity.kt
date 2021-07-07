bc2e3faf949dffd1be4d9d2409ecb03c6c4d4820
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-07 13:59:58 +0800

    App

---
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/OrderListActivity.kt
@@ -0,0 +1,11 @@
+package com.study.app_tickets_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_list)
+    }
+}
