254624bb2b036ec1d0b73796e994299d83b40dde
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-07 12:02:07 +0800

    App

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/TicketsStock.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/TicketsStock.kt
@@ -3,17 +3,11 @@ package com.study.app_tickets_firebase
 class TicketsStock {
     companion object {
         var price: Int = 0
-            set(value) {
-                price = value
-            }
+
         var discount: Double = 0.0
-            set(value) {
-                discount = value
-            }
+
         var totalAmount = 0
-            set(value) {
-                totalAmount = value
-            }
+
         fun subAmount(amount: Int) {
             totalAmount = totalAmount - amount
         }
