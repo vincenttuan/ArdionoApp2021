c6d1aaa8dde74d6c954a31b06632b4eda1d564e4
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-07 11:27:11 +0800

    App

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/MainActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/MainActivity.kt
@@ -2,6 +2,7 @@ package com.study.app_tickets_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun refreshUI() {
-
+        tv_ticket_price.setText(TicketsStock.price.toString())
+        tv_ticket_discount.setText((TicketsStock.discount * 10).toString())
+        tv_total_amount.setText(TicketsStock.totalAmount.toString())
     }
 }
