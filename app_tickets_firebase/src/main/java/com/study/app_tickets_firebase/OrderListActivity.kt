673cb0be885a57ef4382ac25f07747107a87624e
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-08 14:37:02 +0800

    App

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/OrderListActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/OrderListActivity.kt
@@ -1,5 +1,6 @@
 package com.study.app_tickets_firebase
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,10 +19,12 @@ class OrderListActivity : AppCompatActivity() {
     val database = Firebase.database
     val myRef = database.getReference("ticketsStock")
     lateinit var userName: String
-
+    lateinit var context: Context
+    lateinit var recyclerViewAdapter: RecyclerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_list)
+        context = this
         // 取得上一頁傳來的 userName 參數資料
         userName = intent.getStringExtra("userName").toString()
         title = "Hi " + userName + " 的雲端購票紀錄"
@@ -42,6 +45,15 @@ class OrderListActivity : AppCompatActivity() {
             }
         })
 
+        // init recycler view
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(context)
+            recyclerViewAdapter = RecyclerViewAdapter()
+            adapter = recyclerViewAdapter
+            // 分隔線
+            val divider = DividerItemDecoration(context, StaggeredGridLayoutManager.VERTICAL)
+            addItemDecoration(divider)
+        }
 
     }
 
