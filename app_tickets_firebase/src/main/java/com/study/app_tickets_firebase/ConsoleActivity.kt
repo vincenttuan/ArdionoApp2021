b51549896fdc2b2ba2c31cb7ae28e57e62032b90
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-09 09:59:14 +0800

    App

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/ConsoleActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/ConsoleActivity.kt
@@ -84,12 +84,17 @@ class ConsoleActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menu?.add("訂單細目")
-        menu?.add("離開")
+        // 1, 1, 10, "訂單細目" -> group id, item id, order id, 名稱
+        menu?.add(0, 1, 10, "訂單細目")?.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+        menu?.add(0, 2, 20, "返回")?.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            1 -> {} // "訂單細目"
+            2 -> finish() // "返回"
+        }
         return super.onOptionsItemSelected(item)
     }
 
