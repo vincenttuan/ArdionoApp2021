ec07ae677f3f0c0d3ffe1a5e1054e0b0a0155811
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-09 13:39:54 +0800

    App

--- app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/ConsoleActivity.kt
+++ app_tickets_firebase/src/main/java/com/study/app_tickets_firebase/ConsoleActivity.kt
@@ -2,13 +2,12 @@ package com.study.app_tickets_firebase
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -16,6 +15,9 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_console.*
 import kotlinx.android.synthetic.main.activity_order_list.*
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.nio.charset.Charset
 
 class ConsoleActivity : AppCompatActivity() {
     val database = Firebase.database
@@ -71,6 +73,36 @@ class ConsoleActivity : AppCompatActivity() {
             override fun onCancelled(error: DatabaseError) {
             }
         })
+
+        // 載入圖表
+        loadChart()
+    }
+
+    fun loadChart() {
+        var webSettings =  web_view.settings;
+        webSettings.setJavaScriptEnabled(true); // 啟用 Javascript
+        webSettings.setBuiltInZoomControls(true); // 啟用 Zoom
+        var asset_path = "file:///android_asset/";
+        var html = getHtml("chart.html");
+        html = String.format(html!!, 10, 20, 30, 40, 50)
+        web_view.loadDataWithBaseURL(asset_path, html!!, "text/html", "utf-8", null);
+        web_view.requestFocusFromTouch();
+    }
+
+    // 取得 html 內容字串
+    private fun getHtml(filename: String): String? {
+        var html: String? = null
+        try {
+            val `in`: InputStream = assets.open(filename)
+            val out = ByteArrayOutputStream()
+            val buffer = ByteArray(`in`.available())
+            `in`.read(buffer) // 讀出
+            out.write(buffer) // 寫入
+            html = String(out.toByteArray(), Charset.forName("UTF-8"))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return html
     }
 
     fun update(view: View) {
