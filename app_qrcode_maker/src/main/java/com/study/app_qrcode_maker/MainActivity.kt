37b27d22b5df5262fa49f41607b375a51ab8f7c4
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-12 14:35:31 +0800

    App

---
+++ app_qrcode_maker/src/main/java/com/study/app_qrcode_maker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.study.app_qrcode_maker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
