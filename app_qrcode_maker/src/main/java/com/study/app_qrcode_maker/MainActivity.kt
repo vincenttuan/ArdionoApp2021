1a957c9da37a6505ac4f297d0563b2455010bcb7
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-12 14:46:04 +0800

    App

--- app_qrcode_maker/src/main/java/com/study/app_qrcode_maker/MainActivity.kt
+++ app_qrcode_maker/src/main/java/com/study/app_qrcode_maker/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.study.app_qrcode_maker
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun generate(view: View) {
+        val contents = et_message.text.toString()
+        val writer = QRCodeWriter()
+        val bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, 512, 512)
+        val width = bitMatrix.width
+        val height = bitMatrix.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        // 將 bitMatrix 繪製到 bitmap
+        for(x in 0 until width) {
+            for(y in 0 until height) {
+                // 有資料黑色, 無資料白色
+                bitmap.setPixel(x, y, if(bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+            }
+        }
+        iv_qrcode.setImageBitmap(bitmap)
     }
 }
